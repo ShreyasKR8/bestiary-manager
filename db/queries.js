85599f060d4a880ec4532938bfcd161cb8b8b7f0
Shreyas <skrravi08@gmail.com>
2026-03-07 22:11:37 +0530

    add query

--- db/queries.js
+++ db/queries.js
@@ -10,7 +10,22 @@ async function logMonsters(){
     console.log(rows);
 }
 
+async function getMonstersOfType(type){
+    const { rows } = await db.query(`
+        SELECT m.*
+        FROM monsters m
+        JOIN monster_type mt ON
+        m.monster_type_id = mt.id
+        WHERE mt.name = $1`,
+        [type]
+    );
+
+    console.log(rows);
+    return rows;
+}
+
 module.exports = [
     logMonsterType,
     logMonsters,
+    getMonstersOfType,
 ];
